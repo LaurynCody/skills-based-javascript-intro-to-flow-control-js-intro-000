739a135095107ba39fd28be947b77ef23a7c5e6f
Lauryn Cody <lauryncody9@gmail.com>
2020-07-14 03:42:34 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -1,8 +1,7 @@
 function basicTeenager(age) {
 if (age>"12")||(age<"20") {
-return `${You are a teenager!}`
+return `${You are a teenager!}`}
 }
-
 function teenager(age) {
 if (age>"12") && (age<"20") }
 {else {return `${You are not a teenager}`
