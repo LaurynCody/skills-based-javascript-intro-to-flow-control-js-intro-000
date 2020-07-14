70cb6eb5fe8ddcc36a5f5ef341d13386b500b798
Lauryn Cody <lauryncody9@gmail.com>
2020-07-14 03:36:59 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -1,19 +1,32 @@
 function basicTeenager(age) {
-
+if (age>"12")||(age<"20")
+return `${You are a teenager!}`
 }
 
 function teenager(age) {
-
-}
+if (age>"12") && (age<"20")
+}return true
 
 function ageChecker(age) {
-
+if (age==="20") {
+  return false
+}
+if (age==="15") {
+  return true
 }
 
 function ternaryTeenager(age) {
-
+  if (age>"12") && age<"20")
+  return `${You are a teenager}`
+  else if (age<"13") || (age>"19")
+  return `${You are not a teenager}`
 }
 
 function switchAge(age) {
+switch(age)
+  Case "34":
+    return "You are an adult"
+  default:
+    return "I'm not sure"
 
 }
